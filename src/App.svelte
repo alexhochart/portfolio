eb410bf6f6aad4f52c198f9e9e39f4869e7e5d80
alexhochart <alexhochart@gmail.com>
2022-11-18 14:42:10 +0100

    new

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
 	export let name;
+	let src = '/favicon.png';
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<img {src} alt="{name} dancing">
 </main>
 
 <style>
